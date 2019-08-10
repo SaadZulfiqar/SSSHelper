f13081eccad03457b00aadbad85ac66063132022
munam <szulfiqar@xavor.com>
2019-08-11 02:37:06 +0500

    changes

--- app-thunk/containers/Home/index.js
+++ app-thunk/containers/Home/index.js
@@ -1,16 +1,131 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Pause';
+import FavoriteIcon from '@material-ui/icons/PauseCircleFilled';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
-export default class Home extends Component {
-  state = {}
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  navigation: {
+    width: 275,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: '250px',
+    boxShadow: 'none',
+    border: '1px solid #d9d9d9',
+    "&:focus": {
+      boxShadow: '5px 10px #888888'
+    }
+  },
+}));
 
-  render() {
-    return (
-        <Button variant="contained" color="primary">
-            Hello World
-        </Button>
-    );
-  }
+function SimpleBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.navigation}
+    >
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+    </BottomNavigation>
+  );
 }
 
+function ButtonAppBar() {
+  const classes = useStyles();
 
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="#fffff">
+        <Toolbar>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" className={classes.title}>
+            GoSaaS
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+function ContainerApp() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        {/* <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid> */}
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            <span>xs=3</span>
+            <SimpleBottomNavigation />
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default class AppBarMeu extends React.Component {
+  render () {
+    return (
+      <div>
+        <ButtonAppBar />
+        <div style={{marginTop:'20px'}}></div>
+        <ContainerApp />
+      </div>
+      );
+  }
+}
